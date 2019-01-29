975eae4394988e7e1ef409f47cd50f976ee46411
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-01-28 23:54:46 -0800

    finish UI

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,12 @@ function Place(name, location, description, image_link){
   this.image = image_link
 }
 
-AllPlaces.prototype.addPlace(place){
-  place.id = this.assignId;
+AllPlaces.prototype.addPlace = function(place){
+  place.id = this.assignId();
   this.listOfPlaces.push(place);
 }
 
-AllPlaces.prototype.findPlace(id){
+AllPlaces.prototype.findPlace = function(id){
   for(var i=0; i< this.listOfPlaces.length; i++){
     if (this.listOfPlaces[i] && this.listOfPlaces[i].id == id){
       return this.listOfPlaces[i]
@@ -29,7 +29,7 @@ AllPlaces.prototype.findPlace(id){
   return false;
 }
 
-AllPlaces.prototype.deletePlace(id){
+AllPlaces.prototype.deletePlace = function(id){
   for(var i=0; i<this.listOfPlaces.length; i++){
     if(this.listOfPlaces[i] && this.listOfPlaces[i].id == id){
       return this.listOfPlaces[i];
@@ -40,18 +40,47 @@ AllPlaces.prototype.deletePlace(id){
 
 var allPlaces = new AllPlaces();
 
+function displayAllPlaces(){
+  var stringToAddHTML = "";
+
+  for (var i = 0; i < allPlaces.listOfPlaces.length; i++) {
+    if (allPlaces.listOfPlaces[i]){
+      stringToAddHTML += "<div id='" + allPlaces.listOfPlaces[i].id + "'><p>" + allPlaces.listOfPlaces[i].name + "<img class='small' src='" + allPlaces.listOfPlaces[i].image + "' alt='Photo of " + allPlaces.listOfPlaces[i].name + "'></p><br></div>";
+    }
+  };
+  console.log(stringToAddHTML);
+  var show = $("#showAll");
+  show.empty();
+  show.html(stringToAddHTML);
+};
+
+function showDetails(id){
+  var place = allPlaces.findPlace(id);
+  $(".name").html(place.name);
+  $(".location").html(place.location);
+  $(".description").html(place.destination);
+  $(".image").attr("src", place.image);
+};
+
+function attachListeners(){
+  $("#showAll").on("click", "div", function(){
+    showDetails(this.id);
+  });
+};
+
 $(document).ready(function(){
+  attachListeners();
 
   $("#form").submit(function(event){
-    event.preventDefault;
+    event.preventDefault();
 
     inputName = $("#name").val();
     inputLocation = $("#location").val();
     inputDescription = $("#description").val();
     inputImage = $("#image").val();
-
-    var onePlace = new Place(inputName, inputLocation, inputDescription, inputImage);
-    allPlaces.addPlace(onePlace);
-
-  });
-});
+    var newPlace = new Place(inputName, inputLocation, inputDescription, inputImage);
+    allPlaces.addPlace(newPlace);
+    console.log(allPlaces);
+    displayAllPlaces();
+  })
+})
